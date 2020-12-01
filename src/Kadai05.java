6ecf70e8edaf02c46f1ee604acee18bc0f4379d5
190078 <190078@e0505-03.JC-21.local>
2020-12-01 16:13:47 +0900

    編集完了

--- src/Kadai05.java
+++ src/Kadai05.java
@@ -8,7 +8,14 @@ public class Kadai05 {
 	 * @return ”z—ñ‚Ì’†‚ÌÅ‘å’l
 	 */
 	int getMax(int[] d) {
-		
-		return -1;
+/*		int [] d;*/
+		d = new int[d.length];
+		int max =d[0];
+		for(int i=0;i<d.length;i++) {
+			if(max<d[i]) {
+				max=d[i];
+			}
+		}
+		return max;
 	}
 }
